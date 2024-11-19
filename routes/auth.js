fb962d2b936ef67c1e29417eaa5160069ea0bdbf
sittuk-kibet <kibetnathan@kabarak.ac.ke>
2024-11-19 20:00:14 +0300

    modified auth.js

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+const express =require('express');
+const { registerUser, loginUser, logoutUser } = require('./controllers/authCountroller');
+const router = express.Router();
+
+//configure routes
+
+//user registration
+router.post('/register', registerUser);
+
+//user login
+router.post('/login', loginUser);
+
+//user logout
+router.get('/logout', logoutUser);
+
+module.exports = router
