29dd4008fb91c5663b87a30c10a038c105389fde
sittuk-kibet <kibetnathan@kabarak.ac.ke>
2024-11-19 19:50:10 +0300

    modified db.js and index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+// import packages
+const express = require('express');
+const MYSQLStore = require('connect-mysql2')(session);
+require('dotenv').config()
+
+const db = require('./config/db')
+const authRoutes = require('./routes/auth')
+// initialize server
+const app = express()
+
+// set-up middleware
+app.use(express.json())
+//set-up session
+app.use(
+    session({
+        key: 'user_sid',
+        secret: proccess.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        Store: new MYSQLStore({}, db)
+    })
+);
+
+//routes
+app.use('./auth', authRoutes)
+// start server
+const port =proccess.env.port;
+
+app.listen(port,() => {
+    console.log(`Server is running on http://localhost:${port}`)
+
+});
